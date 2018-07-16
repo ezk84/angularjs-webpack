46fff309410b3fb80f0e829a3912deba5f142e27
Ezequiel Muns <ezeq.au@gmail.com>
2018-07-16 16:04:13 +0200

    Composable webpack configuration

--- webpack.config.js
+++ webpack.config.js
@@ -1,247 +1,235 @@
-/* eslint-disable */
+/* eslint-env node */
+const path = require('path');
+const merge = require('webpack-merge');
+const autoprefixer = require('autoprefixer');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
 
-'use strict';
+const common = {
+    module: {
+        rules: [{
+            // JS LOADER
+            test: /\.js$/,
+            exclude: /node_modules/,
+            use: [
+                {
+                    // https://github.com/babel/babel-loader
+                    // Transpile .js files using babel-loader
+                    // Compiles ES6 and ES7 into ES5 code
+                    loader: 'babel-loader',
+                },
+            ],
+        }, {
+            // CSS LOADER
+            test: /\.(le|c)ss$/,
+            use: [
+                {
+                    // https://github.com/webpack/css-loader
+                    // Allow loading css through js
+                    loader: 'css-loader',
+                    options: {
+                        sourceMap: true,
+                    },
+                },
+                {
+                    // https://github.com/postcss/postcss-loader
+                    // Postprocess your css with PostCSS plugins
+                    loader: 'postcss-loader',
+                    options: {
+                        sourceMap: true,
+                        plugins: [autoprefixer],
+                    },
+                },
+                {
+                    loader: 'less-loader',
+                    options: {
+                        sourceMap: true,
+                    },
+                },
+            ],
+        }, {
+            // ASSET LOADER
+            // https://github.com/webpack/file-loader
+            // Copy png, jpg, jpeg, gif, svg, woff, woff2, ttf, eot files to output
+            // Rename the file using the asset hash
+            // Pass along the updated reference to your code
+            // You can add here any file extension you want to get copied to your output
+            test: /\.(png|jpg|jpeg|gif|svg|woff|woff2|ttf|eot)$/,
+            loader: 'file-loader',
+        }, {
+            // HTML LOADER
+            // https://github.com/webpack/raw-loader
+            // Allow loading html through js
+            test: /\.html$/,
+            loader: 'raw-loader',
+        }],
+    },
 
-// Modules
-var webpack = require('webpack');
-var autoprefixer = require('autoprefixer');
-var HtmlWebpackPlugin = require('html-webpack-plugin');
-var MiniCssExtractPlugin = require('mini-css-extract-plugin');
-var CopyWebpackPlugin = require('copy-webpack-plugin');
+    /**
+     * Plugins
+     * Reference: https://webpack.js.org/configuration/plugins/
+     * List: https://webpack.js.org/plugins/
+     */
+    plugins: [],
+
+    /**
+     * Dev server configuration
+     * https://webpack.js.org/configuration/dev-server/
+     */
+    devServer: {
+        contentBase: './src/public',
+        stats: 'minimal',
+        host: '0.0.0.0',
+        overlay: true,
+    },
+};
+
+const dev = {
+    mode: 'development',
+    entry: {
+        app: './src/app/app.js',
+    },
+    output: {
+        path: path.join(__dirname, 'dist'),
+        publicPath: '/',
+        filename: '[name].bundle.js',
+        chunkFilename: '[name].bundle.js',
+    },
+    devtool: 'eval-source-map',
+    module: {
+        rules: [{
+            test: /\.js$/,
+            enforce: 'pre',
+            exclude: /node_modules/,
+            use: [{
+                loader: 'eslint-loader',
+                options: {
+                    cache: true,
+                },
+            }],
+        }, {
+            // CSS LOADER
+            // https://github.com/webpack/style-loader
+            // Use style-loader in development.
+            test: /\.(le|c)ss$/,
+            enforce: 'post',
+            use: 'style-loader',
+        }],
+    },
+    plugins: [
+        // Render index.html
+        // https://github.com/ampedandwired/html-webpack-plugin
+        new HtmlWebpackPlugin({
+            template: './src/public/index.html',
+            inject: 'body',
+        }),
+    ],
+};
+
+const prod = {
+    mode: 'production',
+    entry: {
+        app: './src/app/app.js',
+    },
+    output: {
+        path: path.join(__dirname, 'dist'),
+        publicPath: '/',
+        filename: '[name].[hash].js',
+        chunkFilename: '[name].[hash].js',
+    },
+    devtool: 'source-map',
+    module: {
+        rules: [{
+            test: /\.js$/,
+            enforce: 'pre',
+            exclude: /node_modules/,
+            use: [{
+                loader: 'eslint-loader',
+                options: {
+                    cache: true,
+                },
+            }],
+        }, {
+            // CSS LOADER
+            // https://github.com/webpack-contrib/mini-css-extract-plugin
+            // Extract css files in production builds
+            test: /\.(le|c)ss$/,
+            enforce: 'post',
+            use: MiniCssExtractPlugin.loader,
+        }],
+    },
+    plugins: [
+        // Render index.html
+        // https://github.com/ampedandwired/html-webpack-plugin
+        new HtmlWebpackPlugin({
+            template: './src/public/index.html',
+            inject: 'body',
+        }),
+        // Extract css files
+        // https://github.com/webpack-contrib/mini-css-extract-plugin
+        new MiniCssExtractPlugin({
+            filename: 'css/[name].css',
+            chunkFilename: 'css/[id].css',
+        }),
+        // Copy assets from the public folder
+        // https://github.com/kevlened/copy-webpack-plugin
+        new CopyWebpackPlugin([{
+            from: path.join(__dirname, 'src/public'),
+        }]),
+    ],
+};
+
+const test = {
+    mode: 'none',
+    entry: undefined,
+    output: {},
+    devtool: 'inline-source-map',
+    module: {
+        rules: [{
+            // ISTANBUL LOADER
+            // https://github.com/deepsweet/istanbul-instrumenter-loader
+            // Instrument JS files with istanbul-lib-instrument for subsequent code coverage reporting
+            // Skips node_modules and files that end with .spec.js
+            test: /\.js$/,
+            enforce: 'pre',
+            exclude: [/node_modules/, /\.spec\.js$/],
+            use: [{
+                loader: 'istanbul-instrumenter-loader',
+                options: {
+                    esModules: true,
+                },
+            }],
+        }, {
+            // This must run before istanbul to ensure we're linting the right source
+            test: /\.js$/,
+            enforce: 'pre',
+            exclude: [/node_modules/],
+            use: [{
+                loader: 'eslint-loader',
+                options: {
+                    cache: true,
+                },
+            }],
+        }, {
+            // CSS LOADER
+            // Ignore css for testing
+            test: /\.(le|c)ss$/,
+            enforce: 'post',
+            use: 'null-loader',
+        }],
+    },
+};
+
 
 /**
  * Env
  * Get npm lifecycle event to identify the environment
  */
-var ENV = process.env.npm_lifecycle_event;
-var isTest = ENV === 'test' || ENV === 'test-watch';
-var isProd = ENV === 'build';
-var isDev = !isProd;
+const ENV = process.env.npm_lifecycle_event;
+const isTest = ENV === 'test' || ENV === 'test-watch';
+const isProd = ENV === 'build';
 
-module.exports = function makeWebpackConfig() {
-  /**
-   * Config
-   * Reference: http://webpack.github.io/docs/configuration.html
-   * This is the object where all configuration gets set
-   */
-  var config = {};
-
-  /**
-   * Entry
-   * Reference: http://webpack.github.io/docs/configuration.html#entry
-   * Should be an empty object if it's generating a test build
-   * Karma will set this when it's a test build
-   */
-  config.entry = isTest ? void 0 : {
-    app: './src/app/app.js'
-  };
-
-  /**
-   * Mode
-   * Reference: https://webpack.js.org/concepts/mode/
-   * Enables and disables plugins optimized for each mode.
-   */
-  config.mode = isProd? 'production' : 'development';
-
-  /**
-   * Output
-   * Reference: http://webpack.github.io/docs/configuration.html#output
-   * Should be an empty object if it's generating a test build
-   * Karma will handle setting it up for you when it's a test build
-   */
-  config.output = isTest ? {} : {
-    // Absolute output directory
-    path: __dirname + '/dist',
-
-    // Output path from the view of the page
-    // Uses webpack-dev-server in development
-    publicPath: '/',
-
-    // Filename for entry points
-    // Only adds hash in build mode
-    filename: isProd ? '[name].[hash].js' : '[name].bundle.js',
-
-    // Filename for non-entry points
-    // Only adds hash in build mode
-    chunkFilename: isProd ? '[name].[hash].js' : '[name].bundle.js'
-  };
-
-  /**
-   * Devtool
-   * Reference: http://webpack.github.io/docs/configuration.html#devtool
-   * Type of sourcemap to use per build type
-   */
-  if (isTest) {
-    config.devtool = 'inline-source-map';
-  }
-  else if (isProd) {
-    config.devtool = 'source-map';
-  }
-  else {
-    config.devtool = 'eval-source-map';
-  }
-
-  /**
-   * Loaders
-   * Reference: http://webpack.github.io/docs/configuration.html#module-loaders
-   * List: http://webpack.github.io/docs/list-of-loaders.html
-   * This handles most of the magic responsible for converting modules
-   */
-
-  // Initialize module
-  config.module = {
-    rules: [{
-      // JS LOADER
-      // Reference: https://github.com/babel/babel-loader
-      // Transpile .js files using babel-loader
-      // Compiles ES6 and ES7 into ES5 code
-      test: /\.js$/,
-      exclude: /node_modules/,
-      use: [
-        {
-          loader:'babel-loader'
-        },
-        {
-          loader: 'eslint-loader',
-          options: {
-            cache: true
-          }
-        },
-      ],
-    }, {
-      // CSS LOADER
-      // Reference: https://github.com/webpack/css-loader
-      // Allow loading css through js
-      //
-      // Reference: https://github.com/postcss/postcss-loader
-      // Postprocess your css with PostCSS plugins
-      test: /\.(le|c)ss$/,
-      // Reference: https://github.com/webpack-contrib/mini-css-extract-plugin
-      // Extract css files in production builds
-      //
-      // Reference: https://github.com/webpack/style-loader
-      // Use style-loader in development.
-
-      use: [
-        isTest? 'null-loader' : isDev? 'style-loader' : MiniCssExtractPlugin.loader,
-        {
-          loader: 'css-loader',
-          options: {
-            sourceMap: true,
-          },
-        },
-        {
-          loader: 'postcss-loader',
-          options: {
-            sourceMap: true,
-            plugins: [autoprefixer]
-          }
-        },
-        {
-          loader: 'less-loader',
-          options: {
-            sourceMap: true
-          }
-        }
-      ]
-    }, {
-      // ASSET LOADER
-      // Reference: https://github.com/webpack/file-loader
-      // Copy png, jpg, jpeg, gif, svg, woff, woff2, ttf, eot files to output
-      // Rename the file using the asset hash
-      // Pass along the updated reference to your code
-      // You can add here any file extension you want to get copied to your output
-      test: /\.(png|jpg|jpeg|gif|svg|woff|woff2|ttf|eot)$/,
-      loader: 'file-loader'
-    }, {
-      // HTML LOADER
-      // Reference: https://github.com/webpack/raw-loader
-      // Allow loading html through js
-      test: /\.html$/,
-      loader: 'raw-loader'
-    }]
-  };
-
-  // ISTANBUL LOADER
-  // https://github.com/deepsweet/istanbul-instrumenter-loader
-  // Instrument JS files with istanbul-lib-instrument for subsequent code coverage reporting
-  // Skips node_modules and files that end with .spec.js
-  if (isTest) {
-    config.module.rules.push({
-      enforce: 'pre',
-      test: /\.js$/,
-      exclude: [
-        /node_modules/,
-        /\.spec\.js$/
-      ],
-      loader: 'istanbul-instrumenter-loader',
-      query: {
-        esModules: true
-      }
-    })
-  }
-
-  /**
-   * PostCSS
-   * Reference: https://github.com/postcss/autoprefixer-core
-   * Add vendor prefixes to your css
-   */
-   // NOTE: This is now handled in the `postcss.config.js`
-   //       webpack2 has some issues, making the config file necessary
-
-  /**
-   * Plugins
-   * Reference: http://webpack.github.io/docs/configuration.html#plugins
-   * List: http://webpack.github.io/docs/list-of-plugins.html
-   */
-  config.plugins = [];
-
-  // Skip rendering index.html in test mode
-  if (!isTest) {
-    // Reference: https://github.com/ampedandwired/html-webpack-plugin
-    // Render index.html
-    config.plugins.push(
-      new HtmlWebpackPlugin({
-        template: './src/public/index.html',
-        inject: 'body'
-      })
-    );
-
-    if (isProd) {
-      // Reference: https://github.com/webpack-contrib/mini-css-extract-plugin
-      // Extract css files
-      // Disabled when in test mode or not in build mode
-      config.plugins.push(
-        new MiniCssExtractPlugin({
-          filename: 'css/[name].css',
-          chunkFilename: 'css/[id].css'
-        })
-      );
-    }
-  }
-
-  // Add build specific plugins that aren't enabled with mode == production
-  if (isProd) {
-    config.plugins.push(
-      // Copy assets from the public folder
-      // Reference: https://github.com/kevlened/copy-webpack-plugin
-      new CopyWebpackPlugin([{
-        from: __dirname + '/src/public'
-      }])
-    )
-  }
-
-  /**
-   * Dev server configuration
-   * Reference: http://webpack.github.io/docs/configuration.html#devserver
-   * Reference: http://webpack.github.io/docs/webpack-dev-server.html
-   */
-  config.devServer = {
-    contentBase: './src/public',
-    stats: 'minimal',
-    host: '0.0.0.0',
-    overlay: true
-  };
-
-  return config;
-}();
+module.exports =
+    isTest ? merge(common, test) :
+    isProd ? merge(common, prod) : merge(common, dev);
